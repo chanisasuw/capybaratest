74f2302fbfedb1e6e3c00fcd345866349d7685c6
Chanisa Suwannarang <chanisasuw@gmail.com>
2014-03-11 11:00:15 +0700

    add test Home page Create, Rename, Delete

--- spec/feature_spec_helper.rb
+++ spec/feature_spec_helper.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'rspec'
 require 'capybara/rspec'
-require 'capybara-screenshot'
+# require 'capybara-screenshot/capybara'
 require 'rspec/autorun'
 require 'selenium-webdriver'
 Dir.glob(File.dirname(__FILE__) + '/factories/*', &method(:require))
@@ -10,10 +10,14 @@ Dir.glob(File.dirname(__FILE__) + '/factories/*', &method(:require))
 # Capybara configuration
 Capybara.default_driver = :selenium
 # Capybara.save_and_open_page_path = File.dirname(__FILE__) + '/../snapshots'
-Capybara::Screenshot.autosave_on_failure = true
+# Capybara::Screenshot.autosave_on_failure = true
 
 # RSpec configuration
 RSpec.configure do |config|
+  config.failure_color = :red
+  config.tty = true
+  config.color = true
+
   config.before(:all) do
     # Create fixtures
   end
--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -1,23 +1,56 @@
 require File.dirname(__FILE__) + '/../feature_spec_helper'
   
 describe "sign in", :type => :feature do
-  it "should sign me in" do  
-    visit 'http://www.google.com'
-    expect(page).to have_content "Gmail"
-  end  
+	before(:each) do
+		visit "https://fern.cpdev1.rvglobalsoft.net:2083"
+		within("#login_form") do
+			fill_in "user", with: "fern"
+			fill_in "pass", with: "05092532"
+			click_button "Log in"
+		end
+		find(:css, "#icon-whmaddon_rvsitebuilder").click
+		sleep 0.5
+	end
 
-    it "should sign me in" do  
-    visit 'http://www.google.com'
-    expect(page).to have_content "Gsdfmail"
-  end  
+	after(:each) do
+		click_link "Logout"
+		expect(page).to have_content "You have been logged out."
+	end
 
-    it "should sign me in" do  
-    visit 'http://www.google.com'
-    expect(page).to have_content "Gmail"
-  end  
+	describe "Create a new project" do
+		it "should get new project" do  
+	    find(:css, "#createProjectD").click
+	    fill_in "project_name", with: "chanisa1"
+	  	find(:css, ".ui-button", text: "Save").click
+	  	find(:css, "span", text: "HOME").click
+	  	expect(page).to have_content "chanisa1"
+	  end 
 
-    it "should sign me in" do  
-    visit 'http://www.google.com'
-    expect(page).to have_content "Gmail"
-  end  
+	  it "doesn't create a new project if name is existing" do
+	  	find(:css, "#createProjectD").click
+	    fill_in "project_name", with: "chanisa1"
+	  	find(:css, ".ui-button", text: "Save").click
+	  	expect(page).to have_content "Project name is already taken"
+	  	find(:css, ".ui-button-text", text: "Cancel").click
+	  	expect(page).to have_content "Start Your Web-Building Project"
+	  end
+
+	  it "should rename this project" do
+	  	page.find(:css,".btnRenameProject").click
+	  	fill_in "renameProjectName", with: "opportunity"
+	  	find(:css, ".ui-button-text", text: "Save").click
+	  	sleep 3
+	  	expect(page).to have_content "opportunity"
+	  end
+
+	  it "should be delete this project" do
+	  	page.find(:css,".btnRemoveProject").click
+	  	find(:css, "#removeProjectName")
+	  	find(:css, ".ui-button-text", text: "Remove").click
+	  	expect(page).to have_css("#removeProject-message", text: "Project has been successfully removed.")
+	  	find(:css, ".ui-button-text", text: "Close").click
+	  	sleep 3	  	
+	  	expect(page).to have_no_content "opportunity"
+	  end	
+  end
 end  
